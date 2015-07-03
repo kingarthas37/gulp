c1ee6dac66a1f2fd33f72c4d7e203a49af251c5e
ralf <kingarthas37@hotmail.com>
2015-07-03 17:19:44 +0800

    git

--- gulp/task/rev.js
+++ gulp/task/rev.js
@@ -4,24 +4,7 @@ var path = require('path');
 
 var gulp = require('gulp');
 var RevAll = require('gulp-rev-all');
-
-
-
-var spritesmith = require('gulp.spritesmith');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var rev = require('gulp-rev');
-var gulpif = require('gulp-if');
-var _ = require('underscore');
-var xtend = require('xtend');
-var fs = require('fs');
-var newer = require('gulp-newer');
-var size = require('gulp-size');
 var fingerprint = require('gulp-fingerprint');
-var rimraf = require('gulp-rimraf');
-var through = require('through2');
-var merge = require('merge-stream');
-
 
 var args = require('../util/arg-parse');
 var config = require('../../package.json');
@@ -36,12 +19,12 @@ gulp.task('rev',['clean'],function() {
         return;
     }
 
-    var revAll = new RevAll();
+    var revImageAll = new RevAll();
 
     return gulp.src([path.join(config.path.imageDist,'**')])
-        .pipe(revAll.revision())
+        .pipe(revImageAll.revision())
         .pipe(gulp.dest(path.join(config.path.assetMin,'images')))
-        .pipe(revAll.manifestFile())
+        .pipe(revImageAll.manifestFile())
         .pipe(gulp.dest(path.join(config.path.assetMin,'images')))
         .on('end',function() {
             
@@ -54,6 +37,8 @@ gulp.task('rev',['clean'],function() {
                 }))
                 .pipe(gulp.dest(config.path.cssDist))
                 .on('end',function() {
+                    
+                    var revAll = new RevAll({ dontRenameFile: ['images/'] });
                     
                     gulp.src([config.path.cssDist + '**',config.path.jsDist + '**'])
                         .pipe(revAll.revision())
